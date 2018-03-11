555885e388637f1866f612708af9d693e191a2b7
Fabiano Abreu <fabianoabreu90@outlook.com>
2018-03-10 21:54:16 -0300

    adicao de mais uma coluna

--- campo_minado_jogo_janela.py
+++ campo_minado_jogo_janela.py
@@ -6,21 +6,22 @@ from campo_minado_negocio import *
 
 class CampoMinadoJogoJanela:
 
-    __x0 = 25
-    __x1 = 69
-    __x2 = 113
-    __x3 = 157
-    __x4 = 201
-    __x5 = 245
-    __x6 = 289
-    __x7 = 333
+    __x0 = 3
+    __x1 = 47
+    __x2 = 91
+    __x3 = 135
+    __x4 = 179
+    __x5 = 223
+    __x6 = 267
+    __x7 = 311
+    __x8 = 355
 
-    __y0 = 25
-    __y1 = 65
-    __y2 = 105
-    __y3 = 145
-    __y4 = 185
-    __y5 = 225 
+    __y0 = 40
+    __y1 = 80
+    __y2 = 120
+    __y3 = 160
+    __y4 = 200
+    __y5 = 240
 
     def __init__(self, master):
         self.negocio = CampoMinadoNegocio()
@@ -33,6 +34,7 @@ class CampoMinadoJogoJanela:
         self.__label_05 = StringVar()
         self.__label_06 = StringVar()
         self.__label_07 = StringVar()
+        self.__label_08 = StringVar()
         self.__label_10 = StringVar()
         self.__label_11 = StringVar()
         self.__label_12 = StringVar()
@@ -41,6 +43,7 @@ class CampoMinadoJogoJanela:
         self.__label_15 = StringVar()
         self.__label_16 = StringVar()
         self.__label_17 = StringVar()
+        self.__label_18 = StringVar()
         self.__label_20 = StringVar()
         self.__label_21 = StringVar()
         self.__label_22 = StringVar()
@@ -49,6 +52,7 @@ class CampoMinadoJogoJanela:
         self.__label_25 = StringVar()
         self.__label_26 = StringVar()
         self.__label_27 = StringVar()
+        self.__label_28 = StringVar()
         self.__label_30 = StringVar()
         self.__label_31 = StringVar()
         self.__label_32 = StringVar()
@@ -57,6 +61,7 @@ class CampoMinadoJogoJanela:
         self.__label_35 = StringVar()
         self.__label_36 = StringVar()
         self.__label_37 = StringVar()
+        self.__label_38 = StringVar()
         self.__label_40 = StringVar()
         self.__label_41 = StringVar()
         self.__label_42 = StringVar()
@@ -65,6 +70,7 @@ class CampoMinadoJogoJanela:
         self.__label_45 = StringVar()
         self.__label_46 = StringVar()
         self.__label_47 = StringVar()
+        self.__label_48 = StringVar()
         self.__label_50 = StringVar()
         self.__label_51 = StringVar()
         self.__label_52 = StringVar()
@@ -73,6 +79,7 @@ class CampoMinadoJogoJanela:
         self.__label_55 = StringVar()
         self.__label_56 = StringVar()
         self.__label_57 = StringVar()
+        self.__label_58 = StringVar()       
 
         master.title("Campo Minado")
         master.geometry("400x290+500+350")
@@ -94,6 +101,8 @@ class CampoMinadoJogoJanela:
         self.__button_06.place(x=self.__x6, y=self.__y0)
         self.__button_07 = Button(master, width=5, height=2, background="#EFEFEF", borderwidth=1, textvariable=self.__label_07, command=self.__button07_click)
         self.__button_07.place(x=self.__x7, y=self.__y0)
+        self.__button_08 = Button(master, width=5, height=2, background="#EFEFEF", borderwidth=1, textvariable=self.__label_08, command=self.__button08_click)
+        self.__button_08.place(x=self.__x8, y=self.__y0)
 
         self.__button_10 = Button(master, width=5, height=2, background="#EFEFEF", borderwidth=1, textvariable=self.__label_10, command=self.__button10_click)
         self.__button_10.place(x=self.__x0, y=self.__y1)
@@ -111,6 +120,8 @@ class CampoMinadoJogoJanela:
         self.__button_16.place(x=self.__x6, y=self.__y1)
         self.__button_17 = Button(master, width=5, height=2, background="#EFEFEF", borderwidth=1, textvariable=self.__label_17, command=self.__button17_click)
         self.__button_17.place(x=self.__x7, y=self.__y1)
+        self.__button_18 = Button(master, width=5, height=2, background="#EFEFEF", borderwidth=1, textvariable=self.__label_18, command=self.__button18_click)
+        self.__button_18.place(x=self.__x8, y=self.__y1)
 
         self.__button_20 = Button(master, width=5, height=2, background="#EFEFEF", borderwidth=1, textvariable=self.__label_20, command=self.__button20_click)
         self.__button_20.place(x=self.__x0, y=self.__y2)
@@ -128,6 +139,8 @@ class CampoMinadoJogoJanela:
         self.__button_26.place(x=self.__x6, y=self.__y2)
         self.__button_27 = Button(master, width=5, height=2, background="#EFEFEF", borderwidth=1, textvariable=self.__label_27, command=self.__button27_click)
         self.__button_27.place(x=self.__x7, y=self.__y2)
+        self.__button_28 = Button(master, width=5, height=2, background="#EFEFEF", borderwidth=1, textvariable=self.__label_28, command=self.__button28_click)
+        self.__button_28.place(x=self.__x8, y=self.__y2)
 
         self.__button_30 = Button(master, width=5, height=2, background="#EFEFEF", borderwidth=1, textvariable=self.__label_30, command=self.__button30_click)
         self.__button_30.place(x=self.__x0, y=self.__y3)
@@ -145,6 +158,8 @@ class CampoMinadoJogoJanela:
         self.__button_36.place(x=self.__x6, y=self.__y3)
         self.__button_37 = Button(master, width=5, height=2, background="#EFEFEF", borderwidth=1, textvariable=self.__label_37, command=self.__button37_click)
         self.__button_37.place(x=self.__x7, y=self.__y3)
+        self.__button_38 = Button(master, width=5, height=2, background="#EFEFEF", borderwidth=1, textvariable=self.__label_38, command=self.__button38_click)
+        self.__button_38.place(x=self.__x8, y=self.__y3)
 
         self.__button_40 = Button(master, width=5, height=2, background="#EFEFEF", borderwidth=1, textvariable=self.__label_40, command=self.__button40_click)
         self.__button_40.place(x=self.__x0, y=self.__y4)
@@ -162,6 +177,8 @@ class CampoMinadoJogoJanela:
         self.__button_46.place(x=self.__x6, y=self.__y4)
         self.__button_47 = Button(master, width=5, height=2, background="#EFEFEF", borderwidth=1, textvariable=self.__label_47, command=self.__button47_click)
         self.__button_47.place(x=self.__x7, y=self.__y4)
+        self.__button_48 = Button(master, width=5, height=2, background="#EFEFEF", borderwidth=1, textvariable=self.__label_48, command=self.__button48_click)
+        self.__button_48.place(x=self.__x8, y=self.__y4)
 
         self.__button_50 = Button(master, width=5, height=2, background="#EFEFEF", borderwidth=1, textvariable=self.__label_50, command=self.__button50_click)
         self.__button_50.place(x=self.__x0, y=self.__y5)
@@ -179,200 +196,224 @@ class CampoMinadoJogoJanela:
         self.__button_56.place(x=self.__x6, y=self.__y5)
         self.__button_57 = Button(master, width=5, height=2, background="#EFEFEF", borderwidth=1, textvariable=self.__label_57, command=self.__button57_click)
         self.__button_57.place(x=self.__x7, y=self.__y5)
+        self.__button_58 = Button(master, width=5, height=2, background="#EFEFEF", borderwidth=1, textvariable=self.__label_58, command=self.__button58_click)
+        self.__button_58.place(x=self.__x8, y=self.__y5)
     
     def __button00_click(self):
         self.__button_clicked(self.__button_00)
-        self.__label_00.set("0")
-        self.__mensagem_derrota(self.__label_00)
-        
+        self.__label_00.set(self.__tem_bomba((0,0)))
         
     def __button01_click(self):
         self.__button_clicked(self.__button_01)
-        self.__label_01.set("0")
+        self.__label_01.set(self.__tem_bomba((0,1)))
     
     def __button02_click(self):
         self.__button_clicked(self.__button_02)
-        self.__label_02.set("0")
+        self.__label_02.set(self.__tem_bomba((0,2)))
     
     def __button03_click(self):
         self.__button_clicked(self.__button_03)
-        self.__label_03.set("0")
+        self.__label_03.set(self.__tem_bomba((0,3)))
     
     def __button04_click(self):
         self.__button_clicked(self.__button_04)
-        self.__label_04.set("0")
+        self.__label_04.set(self.__tem_bomba((0,4)))
     
     def __button05_click(self):
         self.__button_clicked(self.__button_05)
-        self.__label_05.set("0")
+        self.__label_05.set(self.__tem_bomba((0,5)))
     
     def __button06_click(self):
         self.__button_clicked(self.__button_06)
-        self.__label_06.set("0")
+        self.__label_06.set(self.__tem_bomba((0,6)))
     
     def __button07_click(self):
         self.__button_clicked(self.__button_07)    
-        self.__label_07.set("0")
+        self.__label_07.set(self.__tem_bomba((0,7)))
+    
+    def __button08_click(self):
+        self.__button_clicked(self.__button_08)    
+        self.__label_08.set(self.__tem_bomba((0,8)))
     
     def __button10_click(self):
         self.__button_clicked(self.__button_10)
-        self.__label_10.set("0")
+        self.__label_10.set(self.__tem_bomba((1,0)))
     
     def __button11_click(self):
         self.__button_clicked(self.__button_11)
-        self.__label_11.set("0")
+        self.__label_11.set(self.__tem_bomba((1,1)))
     
     def __button12_click(self):
         self.__button_clicked(self.__button_12)         
-        self.__label_12.set("0")
+        self.__label_12.set(self.__tem_bomba((1,2)))
     
     def __button13_click(self):
         self.__button_clicked(self.__button_13)
-        self.__label_13.set("0")
+        self.__label_13.set(self.__tem_bomba((1,3)))
     
     def __button14_click(self):
         self.__button_clicked(self.__button_14)
-        self.__label_14.set("0")
+        self.__label_14.set(self.__tem_bomba((1,4)))
     
     def __button15_click(self):
         self.__button_clicked(self.__button_15)
-        self.__label_15.set("0")
+        self.__label_15.set(self.__tem_bomba((1,5)))
     
     def __button16_click(self):
         self.__button_clicked(self.__button_16)
-        self.__label_16.set("0")
+        self.__label_16.set(self.__tem_bomba((1,6)))
     
     def __button17_click(self):
         self.__button_clicked(self.__button_17)
-        self.__label_17.set("0")
+        self.__label_17.set(self.__tem_bomba((1,7)))
+    
+    def __button18_click(self):
+        self.__button_clicked(self.__button_18)
+        self.__label_18.set(self.__tem_bomba((1,8)))
     
     def __button20_click(self):
         self.__button_clicked(self.__button_20)
-        self.__label_20.set("0")
+        self.__label_20.set(self.__tem_bomba((2,0)))
     
     def __button21_click(self):
         self.__button_clicked(self.__button_21)
-        self.__label_21.set("0")
+        self.__label_21.set(self.__tem_bomba((2,1)))
     
     def __button22_click(self):
         self.__button_clicked(self.__button_22)
-        self.__label_22.set("0")
+        self.__label_22.set(self.__tem_bomba((2,2)))
     
     def __button23_click(self):
         self.__button_clicked(self.__button_23)
-        self.__label_23.set("0")
+        self.__label_23.set(self.__tem_bomba((2,3)))
     
     def __button24_click(self):
         self.__button_clicked(self.__button_24)
-        self.__label_24.set("0")
+        self.__label_24.set(self.__tem_bomba((2,4)))
     
     def __button25_click(self):
         self.__button_clicked(self.__button_25)
-        self.__label_25.set("0")
+        self.__label_25.set(self.__tem_bomba((2,5)))
     
     def __button26_click(self):
         self.__button_clicked(self.__button_26)
-        self.__label_26.set("0")
+        self.__label_26.set(self.__tem_bomba((2,6)))
     
     def __button27_click(self):
         self.__button_clicked(self.__button_27)
-        self.__label_27.set("0")
+        self.__label_27.set(self.__tem_bomba((2,7)))
+    
+    def __button28_click(self):
+        self.__button_clicked(self.__button_28)
+        self.__label_28.set(self.__tem_bomba((2,8)))
     
     def __button30_click(self):
         self.__button_clicked(self.__button_30)
-        self.__label_30.set("0")
+        self.__label_30.set(self.__tem_bomba((3,0)))
     
     def __button31_click(self):
         self.__button_clicked(self.__button_31)
-        self.__label_31.set("0")
+        self.__label_31.set(self.__tem_bomba((3,1)))
     
     def __button32_click(self):
         self.__button_clicked(self.__button_32)
-        self.__label_32.set("0")
+        self.__label_32.set(self.__tem_bomba((3,2)))
     
     def __button33_click(self):
         self.__button_clicked(self.__button_33)
-        self.__label_33.set("0")
+        self.__label_33.set(self.__tem_bomba((3,3)))
     
     def __button34_click(self):
         self.__button_clicked(self.__button_34)
-        self.__label_34.set("0")
+        self.__label_34.set(self.__tem_bomba((3,4)))
     
     def __button35_click(self):
         self.__button_clicked(self.__button_35)
-        self.__label_35.set("0")
+        self.__label_35.set(self.__tem_bomba((3,5)))
     
     def __button36_click(self):
         self.__button_clicked(self.__button_36)
-        self.__label_36.set("0")
+        self.__label_36.set(self.__tem_bomba((3,6)))
     
     def __button37_click(self):
         self.__button_clicked(self.__button_37)
-        self.__label_37.set("0")
+        self.__label_37.set(self.__tem_bomba((3,7)))
+    
+    def __button38_click(self):
+        self.__button_clicked(self.__button_38)
+        self.__label_38.set(self.__tem_bomba((3,8)))
     
     def __button40_click(self):
         self.__button_clicked(self.__button_40)
-        self.__label_40.set("0")
+        self.__label_40.set(self.__tem_bomba((4,0)))
     
     def __button41_click(self):
         self.__button_clicked(self.__button_41)
-        self.__label_41.set("0")
+        self.__label_41.set(self.__tem_bomba((4,1)))
     
     def __button42_click(self):
         self.__button_clicked(self.__button_42)
-        self.__label_42.set("0")
+        self.__label_42.set(self.__tem_bomba((4,2)))
     
     def __button43_click(self):
         self.__button_clicked(self.__button_43)
-        self.__label_43.set("0")
+        self.__label_43.set(self.__tem_bomba((4,3)))
     
     def __button44_click(self):
         self.__button_clicked(self.__button_44)
-        self.__label_44.set("0")
+        self.__label_44.set(self.__tem_bomba((4,4)))
     
     def __button45_click(self):
         self.__button_clicked(self.__button_45)
-        self.__label_45.set("0")
+        self.__label_45.set(self.__tem_bomba((4,5)))
     
     def __button46_click(self):
         self.__button_clicked(self.__button_46)
-        self.__label_46.set("0")
+        self.__label_46.set(self.__tem_bomba((4,6)))
     
     def __button47_click(self):
         self.__button_clicked(self.__button_47)
-        self.__label_47.set("0")
+        self.__label_47.set(self.__tem_bomba((4,7)))
+    
+    def __button48_click(self):
+        self.__button_clicked(self.__button_48)
+        self.__label_48.set(self.__tem_bomba((4,8)))
     
     def __button50_click(self):
         self.__button_clicked(self.__button_50)
-        self.__label_50.set("0")
+        self.__label_50.set(self.__tem_bomba((5,0)))
     
     def __button51_click(self):
         self.__button_clicked(self.__button_51)
-        self.__label_51.set("0")
+        self.__label_51.set(self.__tem_bomba((5,1)))
     
     def __button52_click(self):
         self.__button_clicked(self.__button_52)
-        self.__label_52.set("0")
+        self.__label_52.set(self.__tem_bomba((5,2)))
     
     def __button53_click(self):
         self.__button_clicked(self.__button_53)
-        self.__label_53.set("0")
+        self.__label_53.set(self.__tem_bomba((5,3)))
     
     def __button54_click(self):
         self.__button_clicked(self.__button_54)
-        self.__label_54.set("0")
+        self.__label_54.set(self.__tem_bomba((5,4)))
     
     def __button55_click(self):
         self.__button_clicked(self.__button_55)
-        self.__label_55.set("0")
+        self.__label_55.set(self.__tem_bomba((5,5)))
     
     def __button56_click(self):
         self.__button_clicked(self.__button_56)
-        self.__label_56.set("0")
+        self.__label_56.set(self.__tem_bomba((5,6)))
     
     def __button57_click(self):
         self.__button_clicked(self.__button_57)
-        self.__label_57.set("0")
+        self.__label_57.set(self.__tem_bomba((5,7)))
+    
+    def __button58_click(self):
+        self.__button_clicked(self.__button_58)
+        self.__label_58.set(self.__tem_bomba((5,8)))
     
 
     def __button_clicked(self, button):
@@ -381,17 +422,19 @@ class CampoMinadoJogoJanela:
         button["borderwidth"] = 2
         button["relief"] = FLAT
     
-    def __mensagem_derrota(self, label):
-        if label.get() == "0":
-            messagebox.showinfo("Info", "Perdeu")
+    def __mensagem_derrota(self):
+        messagebox.showinfo(messagebox.INFO, "Não foi dessa vez! Você perdeu...")
         
-
-    def __tem_bomba(self, button):
-        tupla = (0,0)
-        valor = self.negocio.tem_bomba(tupla)
-        return valor    
+    def __tem_bomba(self, tupla):
+        var = ""
+        if (not self.negocio.tem_bomba(tupla)):
+            var = self.negocio.bombas_vizinhas(tupla)
+        else:
+            self.__mensagem_derrota()
+        return var
 
 if __name__ == "__main__":
     janela = Tk()
-    CampoMinadoJogoJanela(janela)
+    jogo = CampoMinadoJogoJanela(janela)
     janela.mainloop()
+    jogo.negocio.imprimir_bombas()
