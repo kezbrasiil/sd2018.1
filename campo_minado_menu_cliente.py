1b3d3fdde9e3c16c85e3ff18af2673c8ab0f3934
Fabiano Abreu <fabianoabreu90@outlook.com>
2018-03-30 19:57:02 -0300

    renomeando arquivo de campo_minado_menu_janela.py para campo_minado_menu_cliente.py

---
+++ campo_minado_menu_cliente.py
@@ -0,0 +1,38 @@
+from tkinter import *
+from campo_minado_jogo_janela import *
+from campo_minado_negocio import *
+
+class CampoMinadoMenuJanela:
+
+    master = None
+
+    def __init__(self, master):
+        self.master = master
+        self.master.title("Menu")
+        self.master.geometry("250x250")
+        self.master.maxsize(250,250)
+        self.master.minsize(250,250)
+        self.master["background"] = "#343434"
+
+        self.__novo_jogo = Button(master, width=20, height=2, text="Novo Jogo", background="#EFEFEF", borderwidth=1, command=self.__novo_jogo_ou_continuar_click)
+        self.__novo_jogo.place(x=50, y=30)
+
+        self.__continuar = Button(master, width=20, height=2, text="Continuar", background="#EFEFEF", borderwidth=1, command=self.__novo_jogo_ou_continuar_click)
+        self.__continuar.place(x=50, y=100)
+
+        self.__save_existe()
+
+    def __novo_jogo_ou_continuar_click(self):
+        self.master.destroy()
+        janela = Tk()
+        jogo = CampoMinadoJogoJanela(janela, CampoMinadoNegocio.save_existe())
+        janela.mainloop()   
+    
+    def __save_existe(self):
+        if not CampoMinadoNegocio.save_existe():
+            self.__continuar["state"] = DISABLED
+    
+if __name__ == "__main__":
+    janela = Tk()
+    jogo = CampoMinadoMenuJanela(janela)
+    janela.mainloop()
