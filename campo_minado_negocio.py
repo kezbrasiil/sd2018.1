d156f9102d7bbd262d8b4acad0befabb7278d8ce
Fabiano Abreu <fabianoabreu90@outlook.com>
2018-03-30 19:26:33 -0300

    implementacao inicial de socket no servidor

--- campo_minado_negocio.py
+++ campo_minado_negocio.py
@@ -1,12 +1,48 @@
 from random import randint
+from ast import literal_eval
 import os.path
+from socket import socket, AF_INET, SOCK_DGRAM
 
 class CampoMinadoNegocio(object):
+
+    ENCODE = "UTF-8"
+    MAX_BYTES = 65535
+    PORT = 5000
+    HOST = ''
+
     def __init__(self):
+        self.sock = socket(AF_INET, SOCK_DGRAM)
+        self.origem = (self.HOST, self.PORT)
+        self.sock.bind(self.origem)
+
         self.__contador = -1
         self.__bombas = []
         self.__gerar_bombas()
         self.imprimir_bombas()
+
+    def server(self):
+        print("[Servidor Ativo]\n")
+        while True:
+            datos, endereco = self.sock.recvfrom(self.MAX_BYTES)
+            texto = datos.decode(self.ENCODE)
+
+            print("[Mensagem Recebida] " + texto)
+
+            if '(' in texto:
+                tupla = literal_eval(texto)
+                retorno = self.__tem_bomba(tupla)
+                if retorno:
+                    texto = "sim"
+                else:
+                    texto = self.__bombas_vizinhas(tupla)
+            elif texto == "jogador venceu?":
+                texto = self.__vitoria()
+            
+            print("[Enviando Resposta] " + texto)   
+
+            dados = texto.encode(self.ENCODE)
+            self.sock.sendto(dados, endereco)
+            print("[Fim da Requisição]\n")
 
     def __gerar_bombas(self):
         while(len(self.__bombas) < 14):
@@ -16,17 +52,17 @@ class CampoMinadoNegocio(object):
             if tupla not in self.__bombas:
                 self.__bombas.append(tupla)
     
-    def incrementar_jogada(self):
+    def __incrementar_jogada(self):
         self.__contador = self.__contador + 1
         return self.__contador
 
-    def tem_bomba(self, tupla):
+    def __tem_bomba(self, tupla):
         var = False
         if tupla in self.__bombas:
             var = True
         return var
     
-    def bombas_vizinhas(self, tupla):
+    def __bombas_vizinhas(self, tupla):
         lista = []
         lista.append((tupla[0]-1,tupla[1]-1))
         lista.append((tupla[0]-1,tupla[1]))
@@ -40,10 +76,10 @@ class CampoMinadoNegocio(object):
         qtd_bombas = [i for i in self.__bombas if i in lista]
         return str(len(qtd_bombas))
     
-    def vitoria(self):
-        var = False
+    def __vitoria(self):
+        var = "nao"
         if self.__contador == 14:
-            var = True
+            var = "sim"
         return var
 
     def imprimir_bombas(self):
@@ -60,4 +96,6 @@ class CampoMinadoNegocio(object):
         return var
 
 if __name__ == "__main__":
-    pass
+    server = CampoMinadoNegocio()
+    server.server()
+    
