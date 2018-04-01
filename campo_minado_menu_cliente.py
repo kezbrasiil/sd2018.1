b1d3f1fe42c981e1be02c7d3aeda4f71af624cd9
Fabiano Abreu <fabianoabreu90@outlook.com>
2018-03-31 22:48:07 -0300

    implementacao da funcionalidade de salvar e continuar o jogo

--- campo_minado_menu_cliente.py
+++ campo_minado_menu_cliente.py
@@ -1,12 +1,20 @@
 from tkinter import *
-from campo_minado_jogo_janela import *
+from campo_minado_cliente import *
 from campo_minado_servidor import *
 
 class CampoMinadoMenuCliente:
 
     master = None
 
+    ENCODE = "UTF-8"
+    MAX_BYTES = 65535
+    PORT = 5000
+    HOST = "127.0.0.1"
+
     def __init__(self, master):
+        self.sock = socket(AF_INET, SOCK_DGRAM)
+        self.dest = (self.HOST, self.PORT)
+
         self.master = master
         self.master.title("Menu")
         self.master.geometry("250x250")
@@ -14,22 +22,44 @@ class CampoMinadoMenuCliente:
         self.master.minsize(250,250)
         self.master["background"] = "#343434"
 
-        self.__novo_jogo = Button(master, width=20, height=2, text="Novo Jogo", background="#EFEFEF", borderwidth=1, command=self.__novo_jogo_ou_continuar_click)
+        self.__novo_jogo = Button(master, width=20, height=2, text="Novo Jogo", background="#EFEFEF", borderwidth=1, command=self.__novo_jogo_click)
         self.__novo_jogo.place(x=50, y=30)
 
-        self.__continuar = Button(master, width=20, height=2, text="Continuar", background="#EFEFEF", borderwidth=1, command=self.__novo_jogo_ou_continuar_click)
+        self.__continuar = Button(master, width=20, height=2, text="Continuar", background="#EFEFEF", borderwidth=1, command=self.__continuar_jogo_click)
         self.__continuar.place(x=50, y=100)
 
         self.__save_existe()
 
-    def __novo_jogo_ou_continuar_click(self):
+    def __requisicao(self, texto):
+        dados = texto.encode(self.ENCODE)
+        self.sock.sendto(dados, self.dest)
+        dados, endereco = self.sock.recvfrom(self.MAX_BYTES)
+        return dados.decode(self.ENCODE)
+    
+    def __fechar_conexao(self):
+        self.sock.close()
+
+    def __novo_jogo_click(self):
+        texto = "CRIAR NOVO JOGO"
+        self.__requisicao(texto)
+        self.__criar_jogo(False, None)
+    
+    def __continuar_jogo_click(self):
+        texto = "CARREGAR JOGO SALVO"
+        jogadas = self.__requisicao(texto)
+        self.__criar_jogo(True, jogadas)
+    
+    def __criar_jogo(self, flag, jogadas):
+        self.__fechar_conexao()
         self.master.destroy()
         janela = Tk()
-        jogo = CampoMinadoCliente(janela, CampoMinadoServidor.save_existe())
-        janela.mainloop()   
-    
+        jogo = CampoMinadoCliente(janela, flag, jogadas)
+        janela.mainloop()
+
     def __save_existe(self):
-        if not CampoMinadoNegocio.save_existe():
+        texto = "EXISTE ARQUIVO DE SAVE?"
+        resposta = self.__requisicao(texto)
+        if resposta == "nao":
             self.__continuar["state"] = DISABLED
     
 if __name__ == "__main__":
--- campo_minado_servidor.py
+++ campo_minado_servidor.py
@@ -9,16 +9,14 @@ class CampoMinadoServidor(object):
     MAX_BYTES = 65535
     PORT = 5000
     HOST = ''
-
+    __contador = None
+    __bombas = None
+    __arquivo = None
+    
     def __init__(self):
         self.sock = socket(AF_INET, SOCK_DGRAM)
         self.origem = (self.HOST, self.PORT)
         self.sock.bind(self.origem)
-
-        self.__contador = 0
-        self.__bombas = []
-        self.__gerar_bombas()
-        self.imprimir_bombas()
 
     def server(self):
         print("[Servidor Ativo]\n")
@@ -30,24 +28,43 @@ class CampoMinadoServidor(object):
 
             if '(' in texto:
                 tupla = literal_eval(texto)
+                self.__salvar_jogada(tupla)
                 retorno = self.__tem_bomba(tupla)
                 if retorno:
                     texto = "sim"
                     self.__contador = 0
                 else:
                     texto = self.__bombas_vizinhas(tupla)
-            elif texto == "jogador venceu?":
+            elif texto == "JOGADOR VENCEU?":
                 texto = self.__vitoria()
-            elif texto == "numero de jogadas?":
+            elif texto == "NUMERO DE JOGADAS?":
                 texto = str(self.__contador)
                 self.__incrementar_jogada()
-            
-            print("[Enviando Resposta] " + texto)   
+            elif texto == "EXISTE ARQUIVO DE SAVE?":
+                texto = self.__save_existe()
+            elif texto == "CRIAR NOVO JOGO":
+                self.__bombas = []
+                self.__gerar_bombas()
+                self.__criar_save()
+                self.__contador = 0
+                texto = "ok"
+            elif texto == "CARREGAR JOGO SALVO":
+                texto = self.__carregar_partida()
+                self.__contador = 0
+                self.__criar_save()
+
+            print("[Enviando Resposta] " + texto)
 
             dados = texto.encode(self.ENCODE)
             self.sock.sendto(dados, endereco)
 
             print("[Fim da Requisição]\n")
+    
+    def __carregar_partida(self):
+        arq = open('save/bombas.save', 'r')
+        linha = arq.readline() # primeira linha recupera as bombas
+        self.__bombas = literal_eval(linha)
+        return arq.readline() # segunda linha recupera os botoes clicados
 
     def __gerar_bombas(self):
         while(len(self.__bombas) < 14):
@@ -57,14 +74,11 @@ class CampoMinadoServidor(object):
             if tupla not in self.__bombas:
                 self.__bombas.append(tupla)
     
-    def __incrementar_jogada(self):
-        self.__contador = self.__contador + 1
-        return self.__contador
-
     def __tem_bomba(self, tupla):
         var = False
         if tupla in self.__bombas:
             var = True
+            self.__deletar_arquivo()
         return var
     
     def __bombas_vizinhas(self, tupla):
@@ -77,28 +91,41 @@ class CampoMinadoServidor(object):
         lista.append((tupla[0]+1,tupla[1]-1))
         lista.append((tupla[0]+1,tupla[1]))
         lista.append((tupla[0]+1,tupla[1]+1))
-        
         qtd_bombas = [i for i in self.__bombas if i in lista]
         return str(len(qtd_bombas))
     
+    def __incrementar_jogada(self):
+        self.__contador = self.__contador + 1
+        return self.__contador
+
     def __vitoria(self):
         var = "nao"
-        if self.__contador == 14:
+        if self.__contador == 40:
             var = "sim"
             self.__contador = 0
+            self.__deletar_arquivo()
         return var
-
-    def imprimir_bombas(self):
-        arquivo = open('save/bombas.save', 'w+')
-        texto = str(self.__bombas)
-        arquivo.writelines(texto)
-        arquivo.close()
     
-    @staticmethod
-    def save_existe():
-        var = False
+    def __deletar_arquivo(self):
+        self.__arquivo.close()
+        os.remove('save/bombas.save')
+
+    def __criar_save(self):
+        self.__arquivo = open('save/bombas.save', 'w+')
+        texto = str(self.__bombas)
+        self.__arquivo.writelines(texto+"\n")
+        self.__arquivo.close()
+    
+    def __salvar_jogada(self, tupla):
+        self.__arquivo = open('save/bombas.save', 'a')
+        texto = "button"+str(tupla[0])+str(tupla[1])+"_click,"
+        self.__arquivo.writelines(texto)
+        self.__arquivo.close()
+    
+    def __save_existe(self):
+        var = "nao"
         if os.path.isfile('save/bombas.save'):
-            var = True
+            var = "sim"
         return var
 
 if __name__ == "__main__":
