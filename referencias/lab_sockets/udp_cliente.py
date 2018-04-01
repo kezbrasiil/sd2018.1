1a2e0893fb46d74836871872768b673eb34afef9
Marciorx <marcio.mag@outlook.com>
2018-04-01 02:55:13 -0300

    Adicionando laboratório de socket.

---
+++ referencias/lab_sockets/udp_cliente.py
@@ -0,0 +1,27 @@
+u"""Esse módulo possui a implementação de um cliente UDP."""
+from socket import socket, AF_INET, SOCK_DGRAM
+
+ENCODE = "UTF-8"
+MAX_BYTES = 65535
+PORT = 5000
+HOST = '127.0.0.1'
+
+def client():
+    u"""Procedimento responsável para enviar dados para o servidor e receber alguma resposta por conta disso."""
+    
+    text = "Qual o sentido da vida?"
+    data = text.encode(ENCODE)
+
+    sock = socket(AF_INET, SOCK_DGRAM)
+    dest = (HOST, PORT)
+    sock.sendto(data, dest)
+    print("Mensagem enviada, esperando resposta.")
+
+    data, address = sock.recvfrom(MAX_BYTES)
+    text = data.decode(ENCODE)
+
+    print("A resposta foi: " + text)
+    sock.close() #Fechando socket.
+
+ if __name__ == "__main__":
+     client()
